51b164aebe351e5bab5fb40fc4fc8e89e8186e64
Jake Luciani <jake@datastax.com>
2024-09-25 14:02:54 -0400

    v0.5.0

--- jlama.java
+++ jlama.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 20+
-//PREVIEW 
+//PREVIEW
 //RUNTIME_OPTIONS -server -Dstdout.encoding=UTF-8 -Djdk.incubator.vector.VECTOR_ACCESS_OOB_CHECK=0
 //RUNTIME_OPTIONS --add-modules=jdk.incubator.vector --add-exports java.base/sun.nio.ch=ALL-UNNAMED
 //RUNTIME_OPTIONS --enable-native-access=ALL-UNNAMED -XX:+UnlockDiagnosticVMOptions
@@ -8,8 +8,8 @@
 // not possible before https://github.com/jbangdev/jbang/issues/1838 is fixed.
 // COMPILE_OPTIONS -XX:CompilerDirectivesFile=%{https://raw.githubusercontent.com/tjake/Jlama/main/inlinerules.json}%
 
-//DEPS com.github.tjake:jlama-cli:0.4.0
-//DEPS com.github.tjake:jlama-native:0.4.0:${os.detected.name}-${os.detected.arch}
+//DEPS com.github.tjake:jlama-cli:0.5.0
+//DEPS com.github.tjake:jlama-native:0.5.0:${os.detected.name}-${os.detected.arch}
 
 import static java.lang.System.*;
 import com.github.tjake.jlama.cli.JlamaCli;
